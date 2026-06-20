80d5bbfd06ac7f129106b6daa4c55af507e9736b
Prottoy Mankin <prottoy032020@gmail.com>
2026-06-20 23:17:12 +0600

    Initial file setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
