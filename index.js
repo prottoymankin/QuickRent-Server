fe8ed48144e953205fb393de257891f835e4a70d
Prottoy Mankin <prottoy032020@gmail.com>
2026-06-21 19:03:43 +0600

    Added post api for post new property

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    const database = client.db('quickrent_db');
+    
+    const propertyCollection = database.collection('properties');
+
+    app.post('/api/properties', async (req, res) => {
+      const propertyData = req.body;
+      const result = await propertyCollection.insertOne(propertyData);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
