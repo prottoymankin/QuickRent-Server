6f04dc4a8c7befc7f99ef97597f30b36a92a055f
Prottoy Mankin <prottoy032020@gmail.com>
2026-06-25 11:54:42 +0600

    add API to fetch booking requests

--- index.js
+++ index.js
@@ -57,6 +57,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/api/bookings/owner/:id', async (req, res) => {
+      const id = req.params.id;
+      
+      const query = { ownerId: id };
+
+      const result = await bookingCollection
+        .find(query)
+        .sort({createdAt: -1})
+        .toArray();
+
+      res.send(result);
+    });
+
     app.get('/api/favorites/:userId', async (req, res) => {
       const userId = req.params.userId;
 
